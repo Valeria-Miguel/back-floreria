e8dece91134f4d415734e78cce92df737a97efca
Valeria-Miguel <2022371092@uteq.edu.mx>
2025-02-18 15:16:20 -0600

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,14 +4,14 @@ const { db } = require('../../db')  // Importa la conexión a la base de datos
 const userModel = {
     validateCredentials: async (email, password) => {
         try {
-            // Buscar el usuario en la colección 'users' de Firestore
+         
             const userSnapshot = await db.collection('users').where('email', '==', email).limit(1).get();
 
             if (userSnapshot.empty) {
                 return null;  // No se encontró el usuario
             }
 
-            // Obtener el primer documento encontrado
+          
             const userDoc = userSnapshot.docs[0];
             const userData = userDoc.data();
 
@@ -20,13 +20,13 @@ const userModel = {
 
             if (isPasswordValid) {
                 return {
-                    id: userDoc.id,  // Devolver el ID del documento
+                    id: userDoc.id,  // devolver el ID del documento
                     email: userData.email,
                     role: userData.role
                 };
             }
 
-            return null;  // La contraseña no es válida
+            return null;  // la contraseña no es válida
         } catch (error) {
             console.error('Error al validar credenciales:', error);
             throw new Error('Error en la base de datos');
